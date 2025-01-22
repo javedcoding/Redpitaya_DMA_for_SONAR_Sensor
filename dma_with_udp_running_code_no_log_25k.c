221805b220b6fb4f66229caa98e1c45f94d8c00a
Mashnunul Huq <javedcoding@gmail.com>
2025-01-22 16:13:43 +0100

    Changed Redpitaya inside code 25k is different

--- dma_with_udp_running_code_no_log_25k.c
+++ dma_with_udp_running_code_no_log_25k.c
@@ -119,13 +119,24 @@ struct iic_s
 /* Typedefs for data structs: Header and data */
 typedef struct
 {
-	float HeaderLength; //this define the header length for client packet reading
-    float DataLength; //this defines the data length of the current sent packet in bytes (25000*4)+(6*4)
-    float TotalDataBlocks; //this gives how many datablocks per signal will be sent
-	float DataBlockNumber; //this gives the current sent data block number
-	float SampleFrequency; //sampling frequency of the ultrasonic signal
-    float CurrentTime; //this gives the current time inside redpitaya after the udp connection when program starts for sync
-	uint16_t p_data[READ_DATA_SIZE]; //this holds the actual ADC data
+	float HeaderLength; //this define the header length for client packet reading (17*4)Bytes[C]
+	float DataLength; //this defines the data length of the current sent packet in bytes (25000*2)Bytes [C]
+	float Class;  //this defines what type of object is infront of the sensor [V]
+	float DataType; //this has to be the data type currently only ADC [V]
+	float X_Interval; //This is the ADC sample time in nano seconds [C]
+    float Scaling; //There is no usage of this yet [C]	
+	float SampleFrequency; //sampling frequency of the ultrasonic signal [C]
+	float ADCResolution; //Found from Redpitaya documentation sheet [C]
+	float AmbTemperature; //Fixed temperature or coiming from a temperature sensor [C]
+	float MeasDelay; //This is the ADC start Delay for only stopping capturing the sending pulse [C]
+	float Distance; //this gives the sensor detected distance of the object calculated from reflection time of fly [V]
+	float FFTWindowLength; //These fields are conducting with FFT Data only FFT window length [V]
+	float FFTWindowOffsetIndex; //These fields are conducting with FFT Data only FFT window offset value [V]
+	float SWVersion; //This is the constant software version [C]
+	float TotalDataBlocks; //this gives how many datablocks per signal will be sent [C]
+	float DataBlockNumber; //th6is gives the current sent data block number [V]
+    float CurrentTime; //Previously It was Scaling as the scaling factor has no significance in ADC data it has been replaced with current time in the redpitaya os after the udp connection [V]
+	uint16_t p_data[READ_DATA_SIZE]; //this holds the actual ADC data [V]
 }Header_t;
 
 /* Typedefs for data structure: Header and data */
@@ -152,6 +163,8 @@ static udp_t                udp={-1, -1};
 static Header_t             Header_with_data;
 static run_t	            run;
 //static const uint32_t		thread_pause_us = THREAD_PAUSE_US;
+static float				time_of_flight_us;
+static float				distance;
 static volatile bool		run_thread = true;
 static volatile float       current_time = 0; // Global time variable
 pthread_mutex_t				run_meas_mutex;
@@ -172,6 +185,7 @@ void        iic_set_run( run_t run_in, bool param );
 void        log_activities(char *message, int line);
 int         re_initiate_dma_channel();
 void        close_dma_channel();
+void        measure_distance(float *distance_out);
 
 void        *Server_thread();
 void        *Measure_thread();
@@ -327,13 +341,19 @@ void *Measure_thread()
 	initiate_iic();
 		
 	
-    //Here the number of chunks are calculated
-	Header_with_data.SampleFrequency	= (float)(ADC_SAMPLE_FREQUENCY);
+	//Here the number of chunks are calculated and Cinstant for both cases data are being set to the Header
+	Header_with_data.SampleFrequency	= (float)(ADC_SAMPLE_FREQUENCY); //sampling frequency of the ultrasonic signal [C]
     int Total_data_blocks_number        = (DATA_SIZE / READ_DATA_SIZE);
-    //if ((DATA_SIZE % READ_DATA_SIZE) != 0){
-    //    Total_data_blocks_number += 1;
-    //}
+    if ((DATA_SIZE % READ_DATA_SIZE) != 0){
+        Total_data_blocks_number += 1;
+    }
 	Header_with_data.TotalDataBlocks    = (float)(Total_data_blocks_number);
+    Header_with_data.X_Interval = (float)(ADC_SAMPLE_TIME_NS); //This is the ADC sample time in nano seconds [C]
+	Header_with_data.ADCResolution = (float)14; //Found from Redpitaya documentation sheet [C]
+    Header_with_data.AmbTemperature = (float)15; //Should be a constant and measered by something else sensor [C]
+    Header_with_data.Scaling = (float)1; 
+    Header_with_data.MeasDelay = (float)(ADC_START_DELAY_US * 1.5); //This is the ADC start Delay for only stopping capturing the sending pulse 8*64=512 ns / sample then 2 * 0,30 m / 343,2 m/s = 1.748 ms => 1748µs
+    Header_with_data.SWVersion = (float)(SW_VERSION); //This is the constant software version
     
 	
 	// While loop - stay here while in "normal" operation
@@ -353,6 +373,7 @@ void *Measure_thread()
             continue;
         }
 		
+        measure_distance(&distance);
         Header_with_data.CurrentTime = current_time; // Timestamp the data with acquiring finishing time
 		// retval = measure_distance( &distance );
         #ifdef LOG_ACTIVITIES == 1
@@ -369,17 +390,31 @@ void *Measure_thread()
 		switch(run)
 		{
 			case RUN_NONE:
+                //These are giving all the constant or variable type data of the header to be sent
                 Header_with_data.HeaderLength = (float)(sizeof(Header_with_data) - sizeof(Header_with_data.p_data));
                 Header_with_data.DataLength = (uint16_t)(sizeof(Header_with_data.p_data));
+
+                //New data in header to be sent from previous stack
+                Header_with_data.DataType =  (float) 0.0; //this has to be the data type currently only None so 0.0 
+                Header_with_data.Class = (float) 0.0; //this defines what type of object is infront of the sensor [V] currently not used 
                 send_binary( (uint8_t *)&Header_with_data, (uint16_t)(Header_with_data.HeaderLength) );
 				break;
 			case RUN_ADC:
                 //The logic is select the chunk of data and send it, then select the next chunk
+                Header_with_data.DataType =  (float) 1.0; //this has to be the data type currently only ADC so 1.0
+                Header_with_data.Class = (float) 0.0;  //this defines what type of object is infront of the sensor [V] currently not used 
+                Header_with_data.FFTWindowLength = (float) 0.0; //These fields are conducting with FFT Data only FFT window length [V] currently not used and 0
+                Header_with_data.FFTWindowOffsetIndex = (float) 0.0;  //These fields are conducting with FFT Data only FFT window Offset Index [V] currently not used and 0
                 for (int block_number = 0; block_number < Total_data_blocks_number; block_number++){
                     Header_with_data.DataBlockNumber = (float) block_number;
                     read_data((int16_t *)pData, block_number);
                     Header_with_data.HeaderLength = (float)(sizeof(Header_with_data) - sizeof(Header_with_data.p_data));
                     Header_with_data.DataLength = (uint16_t)(sizeof(Header_with_data.p_data));
+
+                    //New data in header to be sent from previous stack
+                    
+                    Header_with_data.Distance = (float)(distance);
+
                     memcpy( (uint16_t *)Header_with_data.p_data, pData, READ_DATA_SIZE * sizeof(uint16_t));
 			        send_binary( (uint8_t *)&Header_with_data, (uint16_t)(Header_with_data.HeaderLength + Header_with_data.DataLength) );
                     usleep(10);
@@ -636,7 +671,7 @@ int acquire_data(){
     }
 
     // need to sleep for passing by the sending pulses
-    usleep(ADC_START_DELAY_US * 2);
+    usleep(ADC_START_DELAY_US * 1.5);
 
     /* Start the acquisition */
     if (rp_AcqStart() != RP_OK) {
@@ -673,6 +708,8 @@ int acquire_data(){
         }
     }
 
+    time_of_flight_us = (float)( iic.buf[1]<<8 | iic.buf[2] ); // combining two bytes to form a single 16-bit value
+
     /* Stop the acquisition */
     rp_AcqStop();
     #ifdef LOG_ACTIVITIES == 1
@@ -704,6 +741,22 @@ void read_data(int16_t *data, int block_number){
 	
 }
 
+
+/*
+* @brief	This function measures the distance to an object
+*
+* @details	The function searches the maximum amplitude and
+*			its position. 
+*
+*
+*
+*/
+void measure_distance( float *distance_out )
+{
+	// calculate the distance of the measured object
+	// and write back to distance_out pointer
+	*distance_out = V_SONIC_WAVE * (time_of_flight_us / 2e6);
+}
 
 /*
 *@brief     This is the function to release redpitaya memories 
--- dma_with_udp_running_code_no_log_50k.c
+++ dma_with_udp_running_code_no_log_50k.c
@@ -47,15 +47,17 @@
 #define I2C_RDWR                        0x0707      // This is for I square C data Read and Write address
 
 //Define Sensor frequency and other properties
-#define V_SONIC_WAVE                    (float)(343.2)  // [m/s] Schallgeschwindigkeit in Luft
+#define V_SONIC_WAVE                    (float)(343.2)  // [m/s] velocity of ultrasonic wave in air
 #define DECIMATION_FACTOR		        RP_DEC_64 	// This is the descimation factor or number of points channel 1 will take in 1 second
 
-#define ADC_MAX_SAMPLE_FREQUENCY        125000000                                               // [Hz] --> 125 MHz
-#define ADC_SAMPLE_DECIMATION           64                                                      // [-]
-#define ADC_SAMPLE_FREQUENCY            ( ADC_MAX_SAMPLE_FREQUENCY / ADC_SAMPLE_DECIMATION )
-#define ADC_SAMPLE_TIME                 8               // [ns]
-#define ADC_SAMPLE_TIME_NS              (uint32_t)( ADC_SAMPLE_DECIMATION * ADC_SAMPLE_TIME )   // [ns] --> 8*64=512 ns / sample
-#define ADC_START_DELAY_US              (uint32_t)( 0.30 * 2 * 1e6 / V_SONIC_WAVE )     // [µs] --> 2 * 0,30 m / 343,2 m/s = 1.748 µs
+#define ADC_MAX_SAMPLE_FREQUENCY        125000000                                                       // [Hz] --> 125 MHz
+#define ADC_SAMPLE_DECIMATION           64                                                              // [-]
+#define ADC_SAMPLE_FREQUENCY            ( ADC_MAX_SAMPLE_FREQUENCY / ADC_SAMPLE_DECIMATION )            // 125MHz/64 --> 1.953125MHz
+#define ADC_SAMPLE_TIME                 8                                                               // [ns]
+#define ADC_SAMPLE_TIME_NS              (uint32_t)( ADC_SAMPLE_DECIMATION * ADC_SAMPLE_TIME )           // [ns] --> 8*64=512 ns / sample
+#define ADC_START_DELAY_US              (uint32_t)( 0.30 * 2 * 1e6 / V_SONIC_WAVE )                     // [µs] --> 2 * 0,30 m / 343,2 m/s = 1.748 ms or 1748µs 
+#define ADC_BUFFER_DELAY_US			    (uint32_t)(( ADC_BUFFER_SIZE * ADC_SAMPLE_TIME_NS ) / (1e3))	// [µs] --> (16.384 * 512 ns ) / 1000 = 8.388 µs In current version not used
+#define ADC_MID_US					    (ADC_START_DELAY_US + ( ADC_BUFFER_DELAY_US / 2 ))              // In Current version not used
 
 
 
@@ -97,8 +99,8 @@
 
 //Define the IP address of the connected PC and the port to send data through UDP
 #define UDP_PORT                        61231               // This is the connecting UDP port of the PC
-#define SW_VERSION                      2.00
-// #define LOG_ACTIVITIES                  1                   // This value is 1 while debugging on points and logging otherwise put into comment
+#define SW_VERSION                      2.00                // This is the current software version
+//#define LOG_ACTIVITIES                  1                   // This value is 1 while debugging on points and logging otherwise put into comment
 
 // ***************************************************** //
 // ************ Enumerator Type Definations ************ //
@@ -116,16 +118,30 @@ struct iic_s
     .fileName   = "/dev/i2c-0"
 };
 
-/* Typedefs for data structs: Header and data */
+/* Typedefs for data structs: Header and data 
+    [C]-Denotes its constant type and do not change
+    [V]-Denotes its variable type and changes with every data taking
+*/
 typedef struct
 {
-	float HeaderLength; //this define the header length for client packet reading
-    float DataLength; //this defines the data length of the current sent packet in bytes (25000*4)+(6*4)
-    float TotalDataBlocks; //this gives how many datablocks per signal will be sent
-	float DataBlockNumber; //this gives the current sent data block number
-	float SampleFrequency; //sampling frequency of the ultrasonic signal
-    float CurrentTime; //this gives the current time inside redpitaya after the udp connection when program starts for sync
-	uint16_t p_data[READ_DATA_SIZE]; //this holds the actual ADC data
+	float HeaderLength; //this define the header length for client packet reading (17*4)Bytes[C]
+	float DataLength; //this defines the data length of the current sent packet in bytes (25000*2)Bytes [C]
+	float Class;  //this defines what type of object is infront of the sensor [V]
+	float DataType; //this has to be the data type currently only ADC [V]
+	float X_Interval; //This is the ADC sample time in nano seconds [C]
+    float Scaling; //There is no usage of this yet [C]	
+	float SampleFrequency; //sampling frequency of the ultrasonic signal [C]
+	float ADCResolution; //Found from Redpitaya documentation sheet [C]
+	float AmbTemperature; //Fixed temperature or coiming from a temperature sensor [C]
+	float MeasDelay; //This is the ADC start Delay for only stopping capturing the sending pulse [C]
+	float Distance; //this gives the sensor detected distance of the object calculated from reflection time of fly [V]
+	float FFTWindowLength; //These fields are conducting with FFT Data only FFT window length [V]
+	float FFTWindowOffsetIndex; //These fields are conducting with FFT Data only FFT window offset value [V]
+	float SWVersion; //This is the constant software version [C]
+	float TotalDataBlocks; //this gives how many datablocks per signal will be sent [C]
+	float DataBlockNumber; //th6is gives the current sent data block number [V]
+    float CurrentTime; //Previously It was Scaling as the scaling factor has no significance in ADC data it has been replaced with current time in the redpitaya os after the udp connection [V]
+	uint16_t p_data[READ_DATA_SIZE]; //this holds the actual ADC data [V]
 }Header_t;
 
 /* Typedefs for data structure: Header and data */
@@ -152,6 +168,8 @@ static udp_t                udp={-1, -1};
 static Header_t             Header_with_data;
 static run_t	            run;
 //static const uint32_t		thread_pause_us = THREAD_PAUSE_US;
+static float				time_of_flight_us;
+static float				distance;
 static volatile bool		run_thread = true;
 static volatile float       current_time = 0; // Global time variable
 pthread_mutex_t				run_meas_mutex;
@@ -160,18 +178,19 @@ FILE                        *log_file;
 // ***************************************************** //
 // *************** Function Declarations *************** //
 // ***************************************************** //
-int         initiate_redpitaya_with_dma();
+void        initiate_redpitaya_with_dma();
 void        initiate_udp_connection();
 void        initiate_iic();
-int         acquire_data();
+void        acquire_data();
 void        read_data(int16_t *data, int block_number);
 void        release_resources();
 static void parse_udp_message( char *msg_rx );
 void        send_binary( uint8_t *data_in, uint16_t count );
 void        iic_set_run( run_t run_in, bool param );
 void        log_activities(char *message, int line);
-int         re_initiate_dma_channel();
+void        re_initiate_dma_channel();
 void        close_dma_channel();
+void        measure_distance(float *distance_out);
 
 void        *Server_thread();
 void        *Measure_thread();
@@ -309,13 +328,7 @@ void *Measure_thread()
     uint16_t *pData = ( uint16_t *)&(Header_with_data.p_data);
 	
 	
-	if (initiate_redpitaya_with_dma() < 0){
-        #ifdef LOG_ACTIVITIES == 1
-            log_activities("Initiation of Redpitaya with DMA failed", __LINE__);
-        #endif
-        exit(1);
-    }
-
+	initiate_redpitaya_with_dma();
     #ifdef LOG_ACTIVITIES == 1
         log_activities("Redpitaya Initiated", __LINE__);
     #endif
@@ -327,15 +340,22 @@ void *Measure_thread()
 	initiate_iic();
 		
 	
-    //Here the number of chunks are calculated
-	Header_with_data.SampleFrequency	= (float)(ADC_SAMPLE_FREQUENCY);
+    //Here the number of chunks are calculated and Cinstant for both cases data are being set to the Header
+	Header_with_data.SampleFrequency	= (float)(ADC_SAMPLE_FREQUENCY); //sampling frequency of the ultrasonic signal [C]
     int Total_data_blocks_number        = (DATA_SIZE / READ_DATA_SIZE);
-    //if ((DATA_SIZE % READ_DATA_SIZE) != 0){
-    //    Total_data_blocks_number += 1;
-    //}
+    if ((DATA_SIZE % READ_DATA_SIZE) != 0){
+        Total_data_blocks_number += 1;
+    }
 	Header_with_data.TotalDataBlocks    = (float)(Total_data_blocks_number);
-    
-	
+    Header_with_data.X_Interval = (float)(ADC_SAMPLE_TIME_NS); //This is the ADC sample time in nano seconds [C]
+	Header_with_data.ADCResolution = (float)14; //Found from Redpitaya documentation sheet [C]
+    Header_with_data.AmbTemperature = (float)15; //Should be a constant and measered by something else sensor [C]
+    Header_with_data.Scaling = (float)1; 
+    Header_with_data.MeasDelay = (float)(ADC_START_DELAY_US * 1.5); //This is the ADC start Delay for only stopping capturing the sending pulse 8*64=512 ns / sample then 2 * 0,30 m / 343,2 m/s = 1.748 ms => 1748µs
+    Header_with_data.SWVersion = (float)(SW_VERSION); //This is the constant software version
+
+
+
 	// While loop - stay here while in "normal" operation
 	while( 1 )
 	{
@@ -345,41 +365,49 @@ void *Measure_thread()
 		// counter_ms = (counter_ms + THREAD_PAUSE_MS) % 1500;
 		
 		
-		if (re_initiate_dma_channel() < 0){
-            continue;
-        }
+		re_initiate_dma_channel();
 
-        if (acquire_data() < 0){
-            continue;
-        }
-		
+		acquire_data();
+        measure_distance(&distance);
         Header_with_data.CurrentTime = current_time; // Timestamp the data with acquiring finishing time
 		// retval = measure_distance( &distance );
         #ifdef LOG_ACTIVITIES == 1
             log_activities("Data Acquired", __LINE__);
         #endif
 		
-        char log_message[150];
-        sprintf(log_message, "Total Data Blocks in Header: %.2f", Header_with_data.TotalDataBlocks);
-        #ifdef LOG_ACTIVITIES == 1
-            log_activities(log_message, __LINE__);
-        #endif
+        
 		
 		// the "run" variable is (re-)set by the ServerThread via UDP
 		switch(run)
 		{
 			case RUN_NONE:
+                //These are giving all the constant or variable type data of the header to be sent
                 Header_with_data.HeaderLength = (float)(sizeof(Header_with_data) - sizeof(Header_with_data.p_data));
                 Header_with_data.DataLength = (uint16_t)(sizeof(Header_with_data.p_data));
+
+                //New data in header to be sent from previous stack
+                Header_with_data.DataType =  (float) 0.0; //this has to be the data type currently only None so 0.0 
+                Header_with_data.Class = (float) 0.0; //this defines what type of object is infront of the sensor [V] currently not used 
+                
                 send_binary( (uint8_t *)&Header_with_data, (uint16_t)(Header_with_data.HeaderLength) );
 				break;
 			case RUN_ADC:
                 //The logic is select the chunk of data and send it, then select the next chunk
+                Header_with_data.DataType =  (float) 1.0; //this has to be the data type currently only ADC so 1.0
+                Header_with_data.Class = (float) 0.0;  //this defines what type of object is infront of the sensor [V] currently not used 
+                Header_with_data.FFTWindowLength = (float) 0.0; //These fields are conducting with FFT Data only FFT window length [V] currently not used and 0
+                Header_with_data.FFTWindowOffsetIndex = (float) 0.0;  //These fields are conducting with FFT Data only FFT window Offset Index [V] currently not used and 0
                 for (int block_number = 0; block_number < Total_data_blocks_number; block_number++){
                     Header_with_data.DataBlockNumber = (float) block_number;
                     read_data((int16_t *)pData, block_number);
                     Header_with_data.HeaderLength = (float)(sizeof(Header_with_data) - sizeof(Header_with_data.p_data));
                     Header_with_data.DataLength = (uint16_t)(sizeof(Header_with_data.p_data));
+                    
+                    //New data in header to be sent from previous stack
+                    
+                    Header_with_data.Distance = (float)(distance);
+
+                    //Sending the actual ADC data
                     memcpy( (uint16_t *)Header_with_data.p_data, pData, READ_DATA_SIZE * sizeof(uint16_t));
 			        send_binary( (uint8_t *)&Header_with_data, (uint16_t)(Header_with_data.HeaderLength + Header_with_data.DataLength) );
                     usleep(10);
@@ -465,7 +493,7 @@ void initiate_udp_connection(){
 *
 *
 */
-int initiate_redpitaya_with_dma(){
+void initiate_redpitaya_with_dma(){
     /* Initialise Red Pitaya */
     if (rp_InitReset(false) != RP_OK) {
         #ifdef LOG_ACTIVITIES == 1
@@ -482,11 +510,7 @@ int initiate_redpitaya_with_dma(){
     /* Set up the Deep Memory Acquisition */ 
     uint32_t g_adc_axi_start, g_adc_axi_size;
     rp_AcqAxiGetMemoryRegion(&g_adc_axi_start, &g_adc_axi_size);
-    char log_message[150];
-    sprintf(log_message, "Reserved memory Start 0x%X Size 0x%X", g_adc_axi_start, g_adc_axi_size);
-    #ifdef LOG_ACTIVITIES == 1
-        log_activities(log_message, __LINE__);
-    #endif
+    printf("Reserved memory Start 0x%X Size 0x%X\n", g_adc_axi_start, g_adc_axi_size);
 
     /* Set decimation for both channels */
     if (rp_AcqAxiSetDecimationFactor(DECIMATION_FACTOR) != RP_OK) {
@@ -521,13 +545,10 @@ int initiate_redpitaya_with_dma(){
         #endif
         return -1;
     }
-    #ifdef LOG_ACTIVITIES == 1
-        log_activities("Enable CHA 1 at acquiring data", __LINE__);
-    #endif
+    printf("Enable CHA 1\n");
+
     /* Specify the acquisition trigger level*/
     rp_AcqSetTriggerLevel(RP_T_CH_1, 0);
-
-    return 0;
 }
 
 
@@ -540,7 +561,7 @@ int initiate_redpitaya_with_dma(){
 *
 *
 */
-int re_initiate_dma_channel(){
+void re_initiate_dma_channel(){
     /* Enable DMA on channel 1 */
     if (rp_AcqAxiEnable(RP_CH_1, true)) {
         #ifdef LOG_ACTIVITIES == 1
@@ -548,11 +569,7 @@ int re_initiate_dma_channel(){
         #endif
         return -1;
     }
-    #ifdef LOG_ACTIVITIES == 1
-        log_activities("Enable CHA 1", __LINE__);
-    #endif
-
-    return 0;
+    printf("Enable CHA 1\n");
 }
 
 /*
@@ -581,17 +598,11 @@ void close_dma_channel(){
 void initiate_iic(){
     /* Open I²C port for reading and writing and also turning on the sensor */ 
     if ((iic.fd = open(iic.fileName, O_RDWR)) < 0) {
-        #ifdef LOG_ACTIVITIES == 1
-            log_activities("iic file opening failed", __LINE__);
-        #endif
         exit(1);
     }
 
     /* Set the port options and set the address of the device we wish to speak to through I2C protocol */ 
     if (ioctl(iic.fd, I2C_SLAVE_FORCE, iic.address) < 0) {
-        #ifdef LOG_ACTIVITIES == 1
-            log_activities("iic address failed", __LINE__);
-        #endif
         exit(1);
     }
 }
@@ -622,21 +633,18 @@ void send_binary( uint8_t*data_in, uint16_t count )
 *
 *
 */
-int acquire_data(){
+void acquire_data(){
     // I²C programming - start ultrasonic
     iic.buf[0] = 0;             
     //iic.buf[1] = 0x51;        // measurement of distance
     iic.buf[1] = 0x52;          // measurement of time
 
     if ((write(iic.fd, iic.buf, 2)) != 2) {
-        #ifdef LOG_ACTIVITIES == 1
-            log_activities("iic buffering failed", __LINE__);
-        #endif
-        return -1;
+        exit(1);
     }
 
     // need to sleep for passing by the sending pulses
-    usleep(ADC_START_DELAY_US * 2);
+    usleep(ADC_START_DELAY_US*1.5);
 
     /* Start the acquisition */
     if (rp_AcqStart() != RP_OK) {
@@ -645,9 +653,7 @@ int acquire_data(){
         #endif
         return -1;
     }
-    #ifdef LOG_ACTIVITIES == 1
-        log_activities("ACQ Started", __LINE__);
-    #endif
+    printf("ACQ Started\n");
 
     /* Specify trigger source */
     rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
@@ -673,12 +679,10 @@ int acquire_data(){
         }
     }
 
+    time_of_flight_us = (float)( iic.buf[1]<<8 | iic.buf[2] ); // combining two bytes to form a single 16-bit value
     /* Stop the acquisition */
     rp_AcqStop();
-    #ifdef LOG_ACTIVITIES == 1
-        log_activities("Stop acq", __LINE__);
-    #endif
-    return 0;
+    printf("Stop acq\n");
 }
 
 
@@ -695,13 +699,30 @@ void read_data(int16_t *data, int block_number){
     uint32_t posChA;
     rp_AcqAxiGetWritePointerAtTrig(RP_CH_1, &posChA);
 
-    uint32_t reading_point = (uint32_t) (block_number * READ_DATA_SIZE);
-    posChA = reading_point;
+    uint32_t read_size = (uint32_t) (block_number * READ_DATA_SIZE);
+    posChA = read_size;
 
     uint32_t size1 = READ_DATA_SIZE;
 
     rp_AcqAxiGetDataRaw(RP_CH_1, posChA, &size1, data);
 	
+}
+
+
+/*
+* @brief	This function measures the distance to an object
+*
+* @details	The function searches the maximum amplitude and
+*			its position. 
+*
+*
+*
+*/
+void measure_distance( float *distance_out )
+{
+	// calculate the distance of the measured object
+	// and write back to distance_out pointer
+	*distance_out = V_SONIC_WAVE * (time_of_flight_us / 2e6);
 }
 
 
